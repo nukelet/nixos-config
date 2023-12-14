078bcfdc06e7287b86bc31e993e2fe13e94a8ae6
Vinicius Peixoto <nukelet64@gmail.com>
2023-12-14 03:20:26 -0300

    [hosts/common/optional] Fix typo

--- hosts/common/optional/nginx.nix
+++ hosts/common/optional/nginx.nix
@@ -5,6 +5,6 @@
     security.acme = {
         acceptTerms = true;
         defaults.email = "nukelet64@gmail.com";
-        defautls.server = "https://acme-staging-v02.api.letsencrypt.org/directory";
+        defaults.server = "https://acme-staging-v02.api.letsencrypt.org/directory";
     };
 }
