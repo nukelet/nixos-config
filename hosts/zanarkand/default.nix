4cf45743269d3bc8d7db84a4b74f8e65e39042c4
Vinicius Peixoto <nukelet64@gmail.com>
2023-12-14 02:52:14 -0300

    [hosts] Add zanarkand

---
+++ hosts/zanarkand/default.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+    imports = [
+        ./hardware-configuration.nix
+        ../common/users
+	../common/global
+	# ../common/optional/headscale.nix
+    ];
+
+    networking.hostName = "zanarkand"; # Define your hostname.
+    networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+    # Set your time zone.
+    time.timeZone = "America/Sao_Paulo";
+
+    virtualisation.docker.enable = true;
+
+    system.stateVersion = "23.11";
+}
+
