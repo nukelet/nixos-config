d6eaf5933583a742d3097acbd59a6e94984aad3a
Vinicius Peixoto <nukelet64@gmail.com>
2023-12-20 02:52:53 -0300

    [home/nuke/features/desktop] Add basic Sway support

---
+++ home/nuke/features/desktop/sway/default.nix
@@ -0,0 +1,15 @@
+{ lib, config, pkgs, ... }:
+
+{
+    imports = [
+        ../common/kitty.nix
+        ../common/fonts.nix
+    ];
+    wayland.windowManager.sway = {
+        enable = true;
+        config = rec {
+            modifier = "Mod4";
+            terminal = "kitty";
+        };
+    };
+}
