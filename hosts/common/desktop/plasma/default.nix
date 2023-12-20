c1c5cb6f9cbb65cd3021ebec8a93c267bf368326
Vinicius Peixoto <nukelet64@gmail.com>
2023-12-20 16:43:59 -0300

    [hosts/common/desktop/plasma] Configure KWallet

--- hosts/common/desktop/plasma/default.nix
+++ hosts/common/desktop/plasma/default.nix
@@ -4,5 +4,8 @@
     services.xserver.enable = true;
     services.xserver.displayManager.sddm.enable = true;
     services.xserver.desktopManager.plasma5.enable = true;
+    # Fix issue with kwallet not unlocking
+    security.pam.services.sddm.enableKwallet = true;
+    security.pam.services.login.enableKwallet = true;
 }
 
