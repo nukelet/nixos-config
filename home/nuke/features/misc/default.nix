d542365f3351fc348d9b08476aa9d4df1be91159
Vinicius Peixoto <nukelet64@gmail.com>
2024-01-29 16:27:46 -0300

    [home/nuke/features/misc] Add Obsidian

--- home/nuke/features/misc/default.nix
+++ home/nuke/features/misc/default.nix
@@ -1,5 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
+# ugly temporary hack to fix Obsidian's dependence
+# on an EOL Electron version
+let 
+    obsidian = pkgs.obsidian.override {
+        electron = pkgs.electron_25.overrideAttrs (_: {
+            preFixup = "patchelf --add-needed ${pkgs.libglvnd}/lib/libEGL.so.1 $out/bin/electron"; # NixOS/nixpkgs#272912
+            meta.knownVulnerabilities = [ ]; # NixOS/nixpkgs#273611
+        });
+    };
+in
 {
     home.packages = with pkgs; [
         qbittorrent
@@ -7,5 +17,6 @@
         inkscape
         krita
         spotify
+        obsidian
     ];
 }
