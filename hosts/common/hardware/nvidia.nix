a8a2f5ad30aeb458b9adb595b0de961491b28d5d
Vinicius Peixoto <nukelet64@gmail.com>
2024-03-20 12:01:45 -0300

    [hosts/common/hardware] Try to make the proprietary NVIDIA driver play nice with wayland and sway

--- hosts/common/hardware/nvidia.nix
+++ hosts/common/hardware/nvidia.nix
@@ -11,6 +11,15 @@
     # Load nvidia driver for Xorg and Wayland
     services.xserver.videoDrivers = ["nvidia"];
 
+    environment.sessionVariables = {
+        WLR_RENDERER = "vulkan";
+        WLR_NO_HARDWARE_CURSORS = "1";
+        XWAYLAND_NO_GLAMOR = "1";
+        LIBVA_DRIVER_NAME = "nvidia";
+        GBM_BACKEND = "nvidia-drm";
+        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    };
+
     hardware.nvidia = {
 
         # Modesetting is required.
@@ -29,13 +38,13 @@
         # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
         # Only available from driver 515.43.04+
         # Currently alpha-quality/buggy, so false is currently the recommended setting.
-        open = false;
+        open = true;
 
         # Enable the Nvidia settings menu,
         # accessible via `nvidia-settings`.
         nvidiaSettings = true;
 
         # Optionally, you may need to select the appropriate driver version for your specific GPU.
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
     };
 }
