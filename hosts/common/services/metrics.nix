6ac2fe39892ec558ecccdefc1278cb7654b3e168
Vinicius Peixoto <nukelet64@gmail.com>
2024-05-23 01:09:48 -0300

    [hosts/common/services] Fix ports in metrics module

--- hosts/common/services/metrics.nix
+++ hosts/common/services/metrics.nix
@@ -7,19 +7,19 @@
         node = {
             enable = true;
             enabledCollectors = [ "systemd" ];
-            port = 9002;
+            port = 8002;
         };
     };
 
     services.cadvisor = {
         enable = true;
-        port = 9003;
+        port = 8003;
         listenAddress = "0.0.0.0";
     };
 
     networking.firewall.interfaces = {
         "tailscale0" = {
-            allowedTCPPorts = [ 9002 9003 ];
+            allowedTCPPorts = [ 8002 8003 ];
         };
     };
 }
